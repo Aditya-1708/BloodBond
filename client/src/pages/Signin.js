e4c1d8c337aee8a940577fd8c09833233c4ade0f
unknown <Aditya-1708>
2023-12-29 21:49:15 +0530

    skeleton for signin and signup added

---
+++ client/src/pages/Signin.js
@@ -0,0 +1,13 @@
+//basic skeleton done ps add the styling and animations
+function Signin(){
+    return(
+        <div id="SigninMainContainer">
+            <form>
+                <input type="email">Enter your email</input>
+                <input type="password">Enter your password</input>
+                <button>Sign-in</button>
+            </form>
+        </div>
+    )
+}
+export default Signin;
