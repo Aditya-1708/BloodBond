268dccf7e068a3547cc3ab710f4d6259ac1de3dc
iamabhishekagarwal <iamabhishekagarwal1999@gmail.com>
2023-12-30 13:20:07 +0530

    Configured Tailwind

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,4 @@
-//import react router and stuff 
-import Signin from './pages/Signin';
-import Signup from './pages/Signup';
+
 import NavigationBar from './components/NavigationBar';
 function App() {
   return (
