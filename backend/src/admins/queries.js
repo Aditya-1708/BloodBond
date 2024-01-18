861195b5d5ff78a286db85a6babca2f75d8e8ccc
unknown <Aditya-1708>
2024-01-18 21:12:56 +0530

    SQL queries added signup 50% done

--- backend/src/admins/queries.js
+++ backend/src/admins/queries.js
@@ -1,6 +1,13 @@
-const checkEmailExists = "SELECT * FROM admins WHERE email = $1";
-const addAdmin="INSERT INTO authentication (email, password) VALUES ($1, $2);"
-module.exports={
-    checkEmailExists,
-    addAdmin,
-}
+const checkEmailExistsQuery = "SELECT * FROM admindetails WHERE email = $1;";
+const addEmailForVerificationQuery = "INSERT INTO adminVerification (EmailID, OTP) VALUES ($1, $2) RETURNING *;";
+const getDetailsForVerificationQuery = 'SELECT emailid, otp FROM adminverification WHERE emailid = $1;';
+const addEmailIfSuccessfulQuery = 'INSERT INTO admindetails (EmailID, Password) VALUES ($1, $2);';
+const deleteEmailIfOTPUnsuccessfulQuery = 'DELETE FROM adminverification WHERE emailid = $1;';
+
+module.exports = {
+    checkEmailExistsQuery,
+    addEmailForVerificationQuery,
+    getDetailsForVerificationQuery,
+    addEmailIfSuccessfulQuery,
+    deleteEmailIfOTPUnsuccessfulQuery
+};
--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,32 @@
-
-import NavigationBar from './components/NavigationBar';
+import { useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Firstpage from "./pages/FirstPage";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
+import PrivateRoutes from "./utils/PrivateRoutes";
 function App() {
   return (
-    <>
-    <NavigationBar></NavigationBar>
-    </>
+    <Router>
+      <div>
+        <Routes>
+          <Route
+          exact
+          path="/"
+          element={<Firstpage/>}>
+          </Route>
+          <Route
+          exact
+          path="/Signup"
+          element={<Signup/>}>
+          </Route>
+          <Route
+          exact
+          path="/Signin"
+          element={<Signin/>}>
+          </Route>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
